5ba26053ec41192d36b9eb30ee8b59e6d1d89712
Youcef Bouchemat <adex@DESKTOP-10ARUFT.comparex.dz>
2022-11-08 18:36:58 +0100

    add style in order style component

--- src/Components/OrderCard.tsx
+++ src/Components/OrderCard.tsx
@@ -27,18 +27,23 @@ const OrderCard = props => {
     <View style={styles.container}>
       <View style={styles.secondContainer}>
         <Icon name="box" type="entypo" color={colors.white} size={50} />
-        <Text>
+        <Text style={styles.smallBoldText}>
           {data?.carrier.toUpperCase()} : {data?.trackingId.toUpperCase()}
         </Text>
-        <Text>Expected Delivery : {data?.createdAt}</Text>
+        <Text style={styles.deliveryDateText}>
+          Expected Delivery : {data?.createdAt}
+        </Text>
 
         <View style={styles.adressContainer}>
-          <Text>Address</Text>
-          <Text>
+          <Text style={styles.boldText}>Address</Text>
+          <Text style={styles.regularText}>
             {data?.Address}, {data?.City}
           </Text>
-          <Text>Shipping cost ${data?.shippingCost}</Text>
+          <Text style={styles.regularText}>
+            Shipping cost ${data?.shippingCost}
+          </Text>
         </View>
+        <View style={styles.divider}></View>
 
         <FlatList
           showsVerticalScrollIndicator={false}
@@ -48,8 +53,8 @@ const OrderCard = props => {
           renderItem={({index, item}) => {
             return (
               <View key={index} style={styles.flatlistContainer}>
-                <Text>{item.name}</Text>
-                <Text>x{item.quantity}</Text>
+                <Text style={styles.semiBoldText}>{item.name}</Text>
+                <Text style={styles.semiBoldText}>x{item.quantity}</Text>
               </View>
             );
           }}
@@ -86,13 +91,49 @@ const styles = StyleSheet.create({
     backgroundColor: colors.primaryGreen,
     borderRadius: 10,
     margin: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+
+    elevation: 15,
   },
   secondContainer: {padding: 20, alignItems: 'center'},
   adressContainer: {alignItems: 'center', paddingVertical: 20},
+  divider: {
+    backgroundColor: colors.white,
+    height: 0.5,
+    width: '100%',
+    marginBottom: 20,
+  },
   flatlistContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
   skeletonContainer: {padding: 20, borderRadius: 20},
   sketlon: {backgroundColor: colors.sketelonbg},
+  smallBoldText: {
+    fontFamily: 'NunitoSans-Bold',
+    fontSize: 12,
+    color: colors.white,
+  },
+  boldText: {fontFamily: 'NunitoSans-Bold', fontSize: 15, color: colors.white},
+  regularText: {
+    fontFamily: 'NunitoSans-Regular',
+    fontSize: 15,
+    color: colors.white,
+  },
+  semiBoldText: {
+    fontFamily: 'NunitoSans-SemiBold',
+    fontSize: 15,
+    color: colors.white,
+  },
+  deliveryDateText: {
+    fontFamily: 'NunitoSans-Bold',
+    fontSize: 17,
+    color: colors.white,
+  },
 });
